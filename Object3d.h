654799623a1364db782ac6dabde26dabd69a5dff
takumi sato <takumisato0710@gmail.com>
2022-10-14 09:37:06 +0900

    01_06.ビルボードの実装

--- Object3d.h
+++ Object3d.h
@@ -143,6 +143,10 @@ private: // 静的メンバ変数
 	static VertexPosNormalUv vertices[vertexCount];
 	// 頂点インデックス配列
 	static unsigned short indices[indexCount];
+	//ビルボード行列
+	static XMMATRIX matBillboard;
+	//Y軸回りビルボード行列
+	static XMMATRIX matBillboardY;
 
 private:// 静的メンバ関数
 	/// <summary>
@@ -216,5 +220,7 @@ private: // メンバ変数
 	XMMATRIX matWorld;
 	// 親オブジェクト
 	Object3d* parent = nullptr;
+	
+
 };
 
