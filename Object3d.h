965d272afcd727be0528be6300b75d4180440302
takumi sato <takumisato0710@gmail.com>
2022-10-21 10:57:04 +0900

    02_03.点からジオメトリを生成

--- Object3d.h
+++ Object3d.h
@@ -42,7 +42,7 @@ private: // 定数
 	static const float prizmHeight;			// 柱の高さ
 	static const int planeCount = division * 2 + division * 2;		// 面の数
 	static const int vertexCount = 4;		// 頂点数
-	static const int indexCount = 3 * 2;  //インデックス数
+	//static const int indexCount = 3 * 2;  //インデックス数
 
 public: // 静的メンバ関数
 	/// <summary>
@@ -118,7 +118,7 @@ private: // 静的メンバ変数
 	// 頂点バッファ
 	static ComPtr<ID3D12Resource> vertBuff;
 	// インデックスバッファ
-	static ComPtr<ID3D12Resource> indexBuff;
+	//static ComPtr<ID3D12Resource> indexBuff;
 	// テクスチャバッファ
 	static ComPtr<ID3D12Resource> texbuff;
 	// シェーダリソースビューのハンドル(CPU)
@@ -138,11 +138,11 @@ private: // 静的メンバ変数
 	// 頂点バッファビュー
 	static D3D12_VERTEX_BUFFER_VIEW vbView;
 	// インデックスバッファビュー
-	static D3D12_INDEX_BUFFER_VIEW ibView;
+	//static D3D12_INDEX_BUFFER_VIEW ibView;
 	// 頂点データ配列
 	static VertexPosNormalUv vertices[vertexCount];
 	// 頂点インデックス配列
-	static unsigned short indices[indexCount];
+	//static unsigned short indices[indexCount];
 	//ビルボード行列
 	static XMMATRIX matBillboard;
 	//Y軸回りビルボード行列
